42c361e997404e023a670cb65a3d872d9b7ce50f
Mevin Mathew <mevin.mathew@hotmail.com>
2023-03-12 00:24:25 -0500

    Modified train_test_split.py

--- train_test_split.py
+++ train_test_split.py
@@ -6,8 +6,8 @@ import re
  
 # defining source and destination
 # paths
-src = './CHV_dataset/images/'
-src_a = './CHV_dataset/annotations/'
+src = './orig_datasets/CHV_dataset/images/'
+src_a = './orig_datasets/CHV_dataset/annotations/'
 trg = './yolov7/data/PPEData/{folder}/images/'
 trg_a = './yolov7/data/PPEData/{folder}/labels/'
  
@@ -16,15 +16,15 @@ trg_a = './yolov7/data/PPEData/{folder}/labels/'
 # iterating over all the files in
 # the source directory
 
-f_train = open('./CHV_dataset/data_split/train.txt')
+f_train = open('./orig_datasets/CHV_dataset/data_split/train.txt')
 train_imgs = f_train.read().splitlines()
 f_train.close()
 
-f_val = open('./CHV_dataset/data_split/valid.txt')
+f_val = open('./orig_datasets/CHV_dataset/data_split/valid.txt')
 val_imgs = f_val.read().splitlines()
 f_val.close()
 
-f_test = open('./CHV_dataset/data_split/test.txt')
+f_test = open('./orig_datasets/CHV_dataset/data_split/test.txt')
 test_imgs = f_test.read().splitlines()
 f_test.close()
 
@@ -53,5 +53,3 @@ for fname in test_imgs:
     print(annotation_name)
     shutil.copy2(fname, trg.format(folder='test'))
     shutil.copy2(os.path.join(src_a,annotation_name+'.txt'), trg_a.format(folder='test'))
-
-
