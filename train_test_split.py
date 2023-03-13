f1489d3b1420eae74b1ff3bae4eff7bd5779d737
Mevin Mathew <mevin.mathew@hotmail.com>
2023-03-12 22:49:37 -0400

    Adding modifications for new file structure.

--- train_test_split.py
+++ train_test_split.py
@@ -8,8 +8,8 @@ import re
 # paths
 src = './orig_datasets/CHV_dataset/images/'
 src_a = './orig_datasets/CHV_dataset/annotations/'
-trg = './yolov7/data/PPEData/{folder}/images/'
-trg_a = './yolov7/data/PPEData/{folder}/labels/'
+trg = './yolov7/datasets/CHV_dataset/{folder}/images/'
+trg_a = './yolov7/datasets/CHV_dataset/{folder}/labels/'
  
 # files=os.listdir(src)
  
@@ -35,7 +35,7 @@ for fname in train_imgs:
     annotation_name = re.search(r"(ppe\_\d+)", fname).group(0)
     print('------')
     print(annotation_name)
-    shutil.copy2(fname, trg.format(folder='train'))
+    shutil.copy2(os.path.join('./orig_datasets/',fname), trg.format(folder='train'))
     shutil.copy2(os.path.join(src_a,annotation_name+'.txt'), trg_a.format(folder='train'))
 
 for fname in val_imgs:
@@ -43,13 +43,13 @@ for fname in val_imgs:
     annotation_name = re.search(r"(ppe\_\d+)", fname).group(0)
     print('------')
     print(annotation_name)
-    shutil.copy2(fname, trg.format(folder='valid'))
-    shutil.copy2(os.path.join(src_a,annotation_name+'.txt'), trg_a.format(folder='valid'))
+    shutil.copy2(os.path.join('./orig_datasets/',fname), trg.format(folder='val'))
+    shutil.copy2(os.path.join(src_a,annotation_name+'.txt'), trg_a.format(folder='val'))
 
 for fname in test_imgs:
     print(fname)
     annotation_name = re.search(r"(ppe\_\d+)", fname).group(0)
     print('------')
     print(annotation_name)
-    shutil.copy2(fname, trg.format(folder='test'))
+    shutil.copy2(os.path.join('./orig_datasets/',fname), trg.format(folder='test'))
     shutil.copy2(os.path.join(src_a,annotation_name+'.txt'), trg_a.format(folder='test'))
